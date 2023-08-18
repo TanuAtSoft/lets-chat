1795dd22d0353e500c2e4aed69580fd07de377b7
webdev.sp <webdev.sp@amtexsystems.com>
2023-08-18 11:21:11 +0530

    added socket server removing cors

--- src/context/service.js
+++ src/context/service.js
@@ -3,5 +3,5 @@ import socketio from "socket.io-client";
 // import { SOCKET_URL } from "config";
 
 //export const socket = socketio.connect("ws://localhost:8800");
-export const socket = socketio.connect("https://socket-io-vyle.onrender.com/");
+export const socket = socketio.connect("https://lets-chat-socket.onrender.com");
 export const SocketContext = React.createContext();
